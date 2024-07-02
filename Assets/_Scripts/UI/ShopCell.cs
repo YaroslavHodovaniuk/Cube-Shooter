409db1077c0f163ee373043f0aceec29c2be8aeb
Chuvack1st1 <stasara11@gmail.com>
2024-07-02 22:04:10 +0300

    gugik

--- Assets/_Scripts/UI/ShopCell.cs
+++ Assets/_Scripts/UI/ShopCell.cs
@@ -6,23 +6,28 @@ using UnityEngine.UI;
 public class ShopCell : MonoBehaviour
 {
     [SerializeField] private Image _conteiner;
-    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private TextMeshProUGUI priceText;
     [SerializeField] private Button _buyButton;
     [SerializeField] private Button _equipButton;
 
+    [SerializeField] private Color _equipedcolor;
+    [SerializeField] private Color _unequipedcolor;
+
     private int _index;
-    public void Init(Sprite Icon, int index)   //add icon, not GameObject
+    private GameObject weaponObj;
+    public void Init(GameObject Icon, int index)   //add icon, not GameObject
     {
         _index = index;
-        text.text = Shop.Instance.GetPrice(_index).ToString();
+        priceText.text = Shop.Instance.GetPrice(_index).ToString();
         InitButtons();
-
+        Shop.Instance.CurrentEquipedWeaponHasChanged += ChangeEquipButtonByShop;
         _conteiner.sprite = Icon;
     }
     private void OnDisable()
     {
         _buyButton.onClick.RemoveAllListeners();
         _equipButton.onClick.RemoveAllListeners();
+        Shop.Instance.CurrentEquipedWeaponHasChanged -= ChangeEquipButtonByShop;
     }
     private void InitButtons()
     {
@@ -40,15 +45,50 @@ public class ShopCell : MonoBehaviour
         {
             _buyButton.gameObject.SetActive(true);
         }
+        CheckIsPurchased();
     }
 
     private void OnBuyButtonPressed()
     {
         Shop.Instance.PurchaseWeapon(_index);
+        CheckIsPurchased();
+    }
+    private void CheckIsPurchased()
+    {
         if (Shop.Instance.CheckBoughtWeapon(_index))
         {
             _equipButton.gameObject.SetActive(true);
             _buyButton.gameObject.SetActive(false);
+            text.text = "";
+        }
+    }
+
+    private void OnEquipButtonPressed()
+    {
+        if (Shop.Instance.EquipWeapon(_index))
+        {
+            priceText.text = "";
+            ChangeEquipButton(true); 
+        }
+    }
+
+    private void ChangeEquipButtonByShop()
+    {
+        ChangeEquipButton(false);
+    }
+
+    private void ChangeEquipButton(bool key)
+    {
+        if (key)
+        {
+            priceText.text = "Equiped";
+
+            _equipButton.GetComponent<Image>().color = _equipedcolor;
+        }
+        else
+        {
+            priceText.text = "Equip";
+            _equipButton.GetComponent<Image>().color = _unequipedcolor;
         }
     }
 }
