7250b87e9bcc4a41ab686e9b91fa2820d4b60055
Chuvack1st1 <stasara11@gmail.com>
2024-07-03 17:15:46 +0300

    fix scene

---
+++ Assets/_Scripts/Managers/WindowManager.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowManager : MonoBehaviour
+{
+    [SerializeField] private List<WindowUI> windows;  // 0-startMenu;  1-mapShop; 2-weaponshop; ??-setting
+    [SerializeField] private MoneyCountUI moneyCountUI;
+    private WindowUI _currentWindow;
+
+    private void Start()
+    {
+        for (int i = 0; i < windows.Count; i++)
+        {
+            WindowUI window = windows[i];            
+            window.GoToNextWindowAction += OpenNewWindow;
+            window.m_Index = i;
+        }
+        SetStartWindow();
+        moneyCountUI.Init();
+    }
+    private void OnDisable()
+    {
+        foreach (var window in windows)
+        {
+            window.GoToNextWindowAction -= OpenNewWindow;
+        }
+    }
+    private void SetStartWindow()
+    {
+        foreach(var window in windows)
+        {
+            window.gameObject.SetActive(false);
+        }
+        OpenNewWindow(0);
+    }
+
+    private void OpenNewWindow(int index)
+    {
+        ClosePreviosWindow();
+        _currentWindow = windows[index];
+        _currentWindow.gameObject.SetActive(true);
+        _currentWindow.Init();
+    }
+    private void ClosePreviosWindow()
+    {
+        if (_currentWindow != null)
+        {
+            _currentWindow.gameObject.SetActive(false);
+            _currentWindow = null;
+        }
+    }
+}
---
+++ Assets/_Scripts/UI/WindowView.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+public class WindowView : MonoBehaviour
+{
+    [SerializeField] protected List<Button> m_Buttons;
+
+    public int m_Index;
+
+    public UnityAction<int> GoToNextWindowAction;
+
+    private void OnDisable()
+    {
+        DisableButtons();
+    }
+    public virtual void Init()
+    {
+        InitButtons();
+    }
+    protected virtual void InitButtons()
+    {
+        
+    }
+    protected void DisableButtons()
+    {
+        for (int i = 0; i < m_Buttons.Count; i++)
+        {
+            m_Buttons[i].onClick.RemoveAllListeners();
+        }
+    }
+}
