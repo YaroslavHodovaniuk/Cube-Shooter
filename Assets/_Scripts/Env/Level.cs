24f96fe5aa45cfe0491bd32eb2bfabd271125df2
Chuvack1st1 <stasara11@gmail.com>
2024-07-21 14:02:42 +0300

    fix level

--- Assets/_Scripts/Env/Level.cs
+++ Assets/_Scripts/Env/Level.cs
@@ -2,7 +2,4 @@ using UnityEngine;
 
 public class Level : MonoBehaviour
 {
-    [SerializeField] private Transform _playerSpawnPoint;
-
-    public Transform PlayerSpawnerPoint => _playerSpawnPoint;
 }
