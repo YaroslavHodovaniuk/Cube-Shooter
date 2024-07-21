4f7d9830765e9c7155be7630420c12bb74019e4f
Chuvack1st1 <stasara11@gmail.com>
2024-07-21 19:48:05 +0300

    wave spawner

--- Assets/_Scripts/Managers/Level/LevelUnitManager.cs
+++ Assets/_Scripts/Managers/Level/LevelUnitManager.cs
@@ -1,9 +1,15 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 public class LevelUnitManager : StaticInstance<LevelUnitManager>
 {
+
+    [SerializeField] private int SpawnDelay;
+
+    [SerializeField] private int WaveCount;
 
     public void SpawnPlayer()
     {
@@ -12,10 +18,7 @@ public class LevelUnitManager : StaticInstance<LevelUnitManager>
 
     public void SpawningEnemies()
     {
-        for (int i = 0; i < Environment.Instance.EnemySpawnPoints.Count; i++)
-        {
-            SpawnUnit(0, Environment.Instance.EnemySpawnPoints[i], Environment.Instance.EnemyParent);
-        }
+        StartCoroutine(SpawnEnemyWaveCaroutinre());
     }
 
     void SpawnUnit(int unitID, Transform transform, Transform transformParent)
@@ -30,5 +33,16 @@ public class LevelUnitManager : StaticInstance<LevelUnitManager>
         spawned.SetStats(stats);
 
         Environment.Instance.RegisterUnit(unitID, spawned);
+    }
+    private IEnumerator SpawnEnemyWaveCaroutinre()
+    {
+        for (int i = 0; i < WaveCount; i++)
+        {
+            for (int j = 0; j < Environment.Instance.EnemySpawnPoints.Count; j++)
+            {
+                SpawnUnit(0, Environment.Instance.EnemySpawnPoints[j], Environment.Instance.EnemyParent);
+            }
+            yield return new WaitForSeconds(SpawnDelay);
+        }
     }
 }
